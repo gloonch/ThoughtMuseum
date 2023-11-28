c9c9fc31ebb8fd063f903c9ade918f621c425d34
gloonch <mahdihadian@hotmail.com>
2023-11-29 00:05:05 +0330

    init projects

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='bg-amber-400 '>
+          Thought Museum
+      </h1>
+    </div>
+  );
+}
+
+export default App;
