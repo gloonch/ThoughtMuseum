815ddd87b2da261f361b2c9c8f8008cf3f308c46
gloonch <mahdihadian@hotmail.com>
2023-12-12 19:31:06 +0330

    ACCOUNTINFO_002: account info titles changed and added email

--- client/src/components/Me.jsx
+++ client/src/components/Me.jsx
@@ -52,8 +52,22 @@ export default function Me() {
 
 
     return (
-        <div className='flex flex-col gap-2 items-start p-10 mt-36'>
+        <div className='flex flex-col gap-2 items-start p-10 mt-16'>
+            <h1 className='text-3xl mt-4 ml-[38%] text-center'>Account info</h1>
             <form onSubmit={formik.handleSubmit}>
+
+                <div className='flex flex-row items-center justify-between'>
+                    <div>
+                        <h2 className='text-2xl mt-4'>Email</h2>
+                    </div>
+                </div>
+                <input
+                    className='text-md mb-8 bg-gray-200'
+                    value={user?.email}
+                    disabled={true}
+                    name='email'
+                    type='text' />
+
                 <div className='flex flex-row items-center justify-between'>
                     <div>
                         <h2 className='text-2xl mt-4'>Name</h2>
@@ -65,7 +79,7 @@ export default function Me() {
                     </label>
                 </div>
                 <input
-                    className='text-2xl mb-8'
+                    className='text-md '
                     value={formik.values.name ? formik.values.name : user?.name }
                     onChange={formik.handleChange}
                     onBlur={formik.handleBlur}
@@ -73,6 +87,10 @@ export default function Me() {
                     type='text'
                     placeholder='Name' />
 
+                <div>
+                    <h2 className='text-2xl mt-6'>Background</h2>
+                    <p className='text-gray-500 text-sm'>The starred photo will be your background in Homepage</p>
+                </div>
                 <PhotosUploader addedPhotos={photo} onChange={setPhoto} />
                 <div className='flex flex-col items-center mt-8'>
                     <button type="submit" className='w-4/5 bg-amber-400 py-2 px-8 my-4 text-black'><b>Update</b></button>
