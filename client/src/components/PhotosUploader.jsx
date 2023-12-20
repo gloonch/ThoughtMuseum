c441f224aeaa74c9ca650ac5f840fb1cf162f49c
gloonch <mahdihadian@hotmail.com>
2023-12-20 14:01:25 +0330

    THOUGHT_008: calculate video length while registering

--- client/src/components/PhotosUploader.jsx
+++ client/src/components/PhotosUploader.jsx
@@ -3,6 +3,7 @@ import {useState} from "react";
 
 export default function PhotosUploader({addedPhotos, onChange}) {
     const [photoLink, setPhotoLink] = useState('');
+    const [duration, setDuration] = useState(0)
 
     const addPhotoByLink = async (e) => {
         e.preventDefault();
@@ -50,7 +51,12 @@ export default function PhotosUploader({addedPhotos, onChange}) {
         if ( ext === 'png' || ext === 'jpeg' || ext === 'jpg') {
             return (
                 <>
-                    <img className=' w-full object-cover' src={'http://localhost:4000/uploads/' + link} alt='image' />
+                    {/*TODO: image duration should be read from config file to hold a global value*/}
+                    <img
+                        onLoad={(e) => {setDuration(duration + 10 )}}
+                        className=' w-full object-cover'
+                        src={'http://localhost:4000/uploads/' + link}
+                        alt='image' />
                     <button onClick={(e) => removePhoto(e, link)} className=' cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-70  py-2 px-3 m-2'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -72,10 +78,15 @@ export default function PhotosUploader({addedPhotos, onChange}) {
             )
         }
 
-        if ( ext === 'MOV' || ext === 'mp4' ) {
+        if ( ext === 'mov' || ext === 'MOV' || ext === 'mp4' ) {
             return (
                 <>
-                    <video className=' w-full object-cover' src={'http://localhost:4000/uploads/' + link} autoPlay loop />
+                    <video
+                        onCanPlay={(e) => {setDuration(duration + Math.round(e.target.duration, 1000 ))}}
+                        className=' w-full object-cover'
+                        src={'http://localhost:4000/uploads/' + link}
+                        autoPlay muted loop
+                    />
                     <button onClick={(e) => removePhoto(e, link)} className=' cursor-pointer absolute bottom-1 right-1 text-white bg-black bg-opacity-70  py-2 px-3 m-2'>
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
@@ -112,12 +123,13 @@ export default function PhotosUploader({addedPhotos, onChange}) {
                         {renderIcon(link)}
                     </div>
                 ))}
-                <label className='h-32 w-48 cursor-pointer flex items-center gap-1 justify-center border bg-transparent  p-8 text-gray-600'>
+                <label className='h-32 w-48 cursor-pointer flex flex-col items-center  justify-center border bg-transparent text-gray-600'>
                     <input multiple type='file' className='hidden' onChange={handleUploadPhoto}/>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-8 h-8">
                         <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15m0-3l-3-3m0 0l-3 3m3-3V15" />
                     </svg>
-                    Upload
+
+                    <p className='text-sm'>Duration: {duration}s</p>
                 </label>
             </div>
         </>
